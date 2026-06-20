0729da4ce3328a8174dcd3c54b0dc7b9f83e6ab4
waiwaidongci <786858579@qq.com>
2026-06-20 23:23:30 +0800

    .41104836935399:993eae798904fa21a6f4988bf81c8e5e_6a36aed64482b6713af5c9fe.6a36aed94482b6713af5ca00.6a36aed841e1286295152b2c:Trae CN.T(2026/6/20 23:16:41)

--- js/schemeVersionUI.js
+++ js/schemeVersionUI.js
@@ -4,6 +4,9 @@ var SchemeVersionUI = {
   headerEl: null,
   options: null,
   _stateUnsubscribe: null,
+  _searchQuery: "",
+  _sortBy: "updatedAt",
+  _sortOrder: "desc",
 
   init(containerSelector, options) {
     this.container = document.querySelector(containerSelector);
@@ -40,6 +43,23 @@ var SchemeVersionUI = {
       '<div class="scheme-version-list-header">' +
         '<h3>方案版本管理</h3>' +
       '</div>' +
+      '<div class="scheme-version-toolbar">' +
+        '<div class="scheme-search-box">' +
+          '<input type="text" id="schemeSearchInput" placeholder="按名称搜索方案..." />' +
+          '<button class="scheme-search-clear" id="schemeSearchClear" style="display:none">×</button>' +
+        '</div>' +
+        '<div class="scheme-sort-box">' +
+          '<label>排序：</label>' +
+          '<select id="schemeSortSelect">' +
+            '<option value="updatedAt_desc">更新时间 ↓</option>' +
+            '<option value="updatedAt_asc">更新时间 ↑</option>' +
+            '<option value="partCount_desc">构件数量 ↓</option>' +
+            '<option value="partCount_asc">构件数量 ↑</option>' +
+            '<option value="name_asc">名称 A-Z</option>' +
+            '<option value="name_desc">名称 Z-A</option>' +
+          '</select>' +
+        '</div>' +
+      '</div>' +
       '<div class="scheme-version-list" id="schemeVersionList"></div>';
 
     this.listContainer = this.container.querySelector("#schemeVersionList");
@@ -47,15 +67,40 @@ var SchemeVersionUI = {
 
     var saveAsBtn = this.container.querySelector("#saveAsBtn");
     var newSchemeBtn = this.container.querySelector("#newSchemeBtn");
+    var searchInput = this.container.querySelector("#schemeSearchInput");
+    var searchClear = this.container.querySelector("#schemeSearchClear");
+    var sortSelect = this.container.querySelector("#schemeSortSelect");
 
     saveAsBtn.onclick = function() { this._handleSaveAs(); }.bind(this);
     newSchemeBtn.onclick = function() { this._handleNewScheme(); }.bind(this);
+
+    searchInput.addEventListener("input", function(e) {
+      this._searchQuery = e.target.value.trim();
+      searchClear.style.display = this._searchQuery ? "flex" : "none";
+      this.render();
+    }.bind(this));
+
+    searchClear.onclick = function() {
+      this._searchQuery = "";
+      searchInput.value = "";
+      searchClear.style.display = "none";
+      searchInput.focus();
+      this.render();
+    }.bind(this);
+
+    sortSelect.value = this._sortBy + "_" + this._sortOrder;
+    sortSelect.addEventListener("change", function(e) {
+      var parts = e.target.value.split("_");
+      this._sortBy = parts[0];
+      this._sortOrder = parts[1];
+      this.render();
+    }.bind(this));
   },
 
   render() {
     if (!this.listContainer) return;
 
-    var schemes = SchemeStorage.list();
+    var allSchemes = SchemeStorage.list();
     var currentId = SchemeState.currentSchemeId;
     var state = SchemeState.getState();
 
@@ -69,15 +114,28 @@ var SchemeVersionUI = {
       badgeEl.style.display = state.hasUnsavedChanges ? "inline-block" : "none";
     }
 
-    if (schemes.length === 0) {
+    if (allSchemes.length === 0) {
       this.listContainer.innerHTML =
         '<div class="scheme-empty">暂无保存的方案，点击"保存方案"创建第一个方案。</div>';
       return;
     }
 
+    var filteredSchemes = this._filterSchemes(allSchemes);
+    var sortedSchemes = this._sortSchemes(filteredSchemes);
+
+    if (sortedSchemes.length === 0) {
+      this.listContainer.innerHTML =
+        '<div class="scheme-empty scheme-search-empty">' +
+          '<div class="scheme-empty-icon">🔍</div>' +
+          '<div class="scheme-empty-title">没有找到匹配的方案</div>' +
+          '<div class="scheme-empty-desc">尝试使用其他关键词搜索</div>' +
+        '</div>';
+      return;
+    }
+
     var html = "";
-    for (var i = 0; i < schemes.length; i++) {
-      var s = schemes[i];
+    for (var i = 0; i < sortedSchemes.length; i++) {
+      var s = sortedSchemes[i];
       var isCurrent = s.id === currentId;
       html +=
         '<div class="scheme-item' + (isCurrent ? ' current' : '') + '" data-id="' + s.id + '">' +
@@ -104,6 +162,38 @@ var SchemeVersionUI = {
 
     this.listContainer.innerHTML = html;
     this._bindListEvents();
+  },
+
+  _filterSchemes(schemes) {
+    if (!this._searchQuery) return schemes;
+    var query = this._searchQuery.toLowerCase();
+    var result = [];
+    for (var i = 0; i < schemes.length; i++) {
+      if (schemes[i].name.toLowerCase().indexOf(query) >= 0) {
+        result.push(schemes[i]);
+      }
+    }
+    return result;
+  },
+
+  _sortSchemes(schemes) {
+    var sortBy = this._sortBy;
+    var sortOrder = this._sortOrder;
+    var arr = schemes.slice();
+
+    arr.sort(function(a, b) {
+      var cmp = 0;
+      if (sortBy === "name") {
+        cmp = a.name.localeCompare(b.name, "zh-Hans-CN");
+      } else if (sortBy === "updatedAt") {
+        cmp = a.updatedAt - b.updatedAt;
+      } else if (sortBy === "partCount") {
+        cmp = a.partCount - b.partCount;
+      }
+      return sortOrder === "asc" ? cmp : -cmp;
+    });
+
+    return arr;
   },
 
   _bindListEvents() {
